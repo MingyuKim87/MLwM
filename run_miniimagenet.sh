20daa71d32a01fd52c9dc92136f01d8bd0618670
mgyukim <callingu@kaist.ac.kr>
2020-09-07 10:49:21 +0900

    pretrained ResNet

--- run_miniimagenet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo 'python ./MLwM_miniimagenet_main.py --device=$1 --data_path=/home/osilab/dataset/miniimagenet
--- run_omniglot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo 'python ./MLwM_omniglot_main.py --device=$1 --training_data_path=/home/osilab/dataset/omniglot/images_background --test_data_path=/home/osilab/dataset/omniglot/images_evaluation
